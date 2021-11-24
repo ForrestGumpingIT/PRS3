3b53d773eac795d0f45485374dd699b51ee4c998
ForrestGumpingIT <92694825+ForrestGumpingIT@users.noreply.github.com>
2021-11-24 15:11:10 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function count(){
+	for(i=0;i<=3;i=i+1){
+		
+function check () {
+//declaring variables
+var ans=document.getElementById('ans').value;
+
+
+document.getElementById("main").innerHTML+="<br/>"+count+"	
+		
+		if (ans=="Rock"){
+			document.getElementById("placeholder").innerHTML="You lose! Computer chose Paper!";
+		}
+		else if (ans=="Paper"){
+		    document.getElementById("placeholder").innerHTML="You win! Computer chose Rock!";
+      	}
+		else if (ans=="Scissors"){
+			document.getElementById("placeholder").innerHTML="You win! Computer chose Paper!";
+        }
+		else{ 
+		alert("Invalid input, please try again");
+		}
+		
+	function hide(){
+	document.getElementById("Button").style.visibility="none";
+}	
+}
+	}
